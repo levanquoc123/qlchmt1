ce47ac8e654866a6f98d61e38944a93066880f77
levanquoc123 <43307691+levanquoc123@users.noreply.github.com>
2020-11-08 13:02:41 +0700

    1

--- qlchmt/lib/main.dart
+++ qlchmt/lib/main.dart
@@ -1,23 +1,96 @@
 import 'package:flutter/material.dart';
-import 'package:qlchmt/dangnhap/login.dart';
+import 'package:qlchmt/view/loginPage.dart';
+import 'package:shared_preferences/shared_preferences.dart';
+
+import 'view/loginPage.dart';
 
 void main() => runApp(MyApp());
 
 class MyApp extends StatelessWidget {
   @override
+
   Widget build(BuildContext context) {
     return MaterialApp(
       title: 'Shop Computer',
       theme: ThemeData(
         primarySwatch: Colors.blue,
       ),
-      home: LoginPage(),
-        //body: Center(
-        //  child: Text('Shop Computer', style: TextStyle(fontSize: 30), ),
-        //  ),
-        
+      home: MainPage(),        
     );
   }
 }
 
-//class LoginPage extends StatefulBuilder
+class MainPage extends StatefulWidget {
+  @override
+  _MainPageState createState() => _MainPageState();
+}
+
+
+class _MainPageState extends State<MainPage> {
+  SharedPreferences sharedPreferences;
+
+  @override
+  void initState() {
+    super.initState();
+    checkLoginStatus();
+  }
+
+  checkLoginStatus() async {
+    sharedPreferences = await SharedPreferences.getInstance();
+    if(sharedPreferences.getString("token") == null) {
+      Navigator.of(context).pushAndRemoveUntil(MaterialPageRoute(builder: (BuildContext context) => LoginPage()), (Route<dynamic> route) => false);
+    }
+  }
+
+  @override
+  Widget build(BuildContext context) {
+    return Scaffold(
+      appBar: AppBar(
+        title: Text("Nodejs-Mongodb", style: TextStyle(color: Colors.white)),
+        actions: <Widget>[
+          FlatButton(
+              onPressed: () {
+              sharedPreferences.clear();
+              sharedPreferences.commit();
+              Navigator.of(context).pushAndRemoveUntil(MaterialPageRoute(builder: (BuildContext context) => MainPage()), (Route<dynamic> route) => false);
+            },
+            child: Text("Log Out", style: TextStyle(color: Colors.white)),)
+        ],
+      ),
+      body: Center(child: Text("Main Page")),
+      drawer: Drawer(
+        child: new ListView(
+              children: <Widget>[
+                new UserAccountsDrawerHeader(
+                  accountName: new Text('Ejercicios'),
+                  accountEmail: new Text('codigoalphacol@gmail.com'),
+                ),
+                /*
+               new ListTile(
+                  title: new Text("List Products"),
+                  trailing: new Icon(Icons.help),
+                  onTap: () => Navigator.of(context).push(new MaterialPageRoute(
+                    builder: (BuildContext context) => ListProducts(),
+                  )),
+                  ),
+                new ListTile(
+                  title: new Text("Add Products"),
+                  trailing: new Icon(Icons.help),
+                 onTap: () => Navigator.of(context).push(new MaterialPageRoute(
+                    builder: (BuildContext context) => AddDataProduct(),
+                  )),
+                  ),                
+                 new Divider(),
+                new ListTile(
+                  title: new Text("Register user"),
+                  trailing: new Icon(Icons.fitness_center),
+                  onTap: () => {},                  
+                ),*/
+              ],
+        ),
+      ),
+    );
+  }
+
+
+}
